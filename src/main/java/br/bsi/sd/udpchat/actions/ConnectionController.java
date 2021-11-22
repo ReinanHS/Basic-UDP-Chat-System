0a48ceee62ba9344636e44b15af11ffc7f513e6c
Reinan <reinangabriel1520@gmail.com>
2021-11-21 21:09:41 -0300

    feat: add server connection

--- src/main/java/br/bsi/sd/udpchat/actions/ConnectionController.java
+++ src/main/java/br/bsi/sd/udpchat/actions/ConnectionController.java
@@ -1,13 +1,20 @@
 package br.bsi.sd.udpchat.actions;
 
 import br.bsi.sd.udpchat.UdpChatApp;
+import br.bsi.sd.udpchat.utils.ClienteUDP;
+import br.bsi.sd.udpchat.utils.ServeUDP;
 
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class ConnectionController extends ControllerBase{
     private String username;
     private String ipConnection;
+
+    private String myInet4Address;
 
     @Override
     public void command(String[] args) {
@@ -17,12 +24,67 @@ public class ConnectionController extends ControllerBase{
 
         this.ipConnection = args[1];
         this.username = System.getProperty("user.name");
+        this.myInet4Address = this.getInet4Address();
 
         if(args.length <= 2) {
             this.getConfigUserName();
         }
 
         System.out.println(this.username + " we will connect to IP "+ this.ipConnection);
+        System.out.println("Your ip address is "+ this.myInet4Address);
+
+        try {
+            this.runChat();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method to get the customer's ip address
+     * @return ip
+     */
+    private String getInet4Address() {
+        try {
+            return Inet4Address.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "";
+        }
+    }
+
+    public void runChat() throws Exception {
+        boolean isExitChat = false;
+        Scanner scanner = new Scanner(System.in);
+        ClienteUDP clienteUDP = new ClienteUDP();
+
+        Thread threadServidor = new Thread() {
+            @Override
+            public void run() {
+                try {
+
+                    ServeUDP serveUDP = new ServeUDP();
+                    serveUDP.onText();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        while (!isExitChat) {
+            System.out.print(this.username+"@"+this.myInet4Address+": ");
+            String text = scanner.next();
+
+            if(text.toLowerCase(Locale.ROOT).equals("\\sair") || text.toLowerCase(Locale.ROOT).equals("\\exit")) {
+                isExitChat = true;
+            } else {
+                clienteUDP.sendText(this.ipConnection, this.username+"@"+this.myInet4Address+": "+text);
+            }
+        }
+
+        if(isExitChat) {
+            threadServidor.stop();
+        }
     }
 
     /**
